33b5926c58a21c135b5397f3472ae268ac261f86
Panndora1 <valentina_pavlovna1881@mail.ru>
2022-05-02 17:14:21 +0400

    feat: add remove from list and stack

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList( l, k ) {
+  let acc = l;
+  let index = 0;
+
+  while ( l.value == k) {
+    l = l.next;
+  }
+
+  while(acc.next) {
+  //  console.log(acc.value)
+    if(acc.next.value == k) {
+      let position = index;
+     // console.log(position)
+
+      if (position == 0) {
+        acc = acc.next;
+        //console.log(acc)
+      } else {
+        acc.next = acc.next.next;
+        console.log(acc)
+      }
+    } else {
+      acc = acc.next;
+      index++
+    }
+    
+  }
+
+
+  return l
 }
 
 module.exports = {
